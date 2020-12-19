c682e8f99988259cea6652f79f184afc03a17434
masujima <masujima.ernar@gmail.com>
2020-12-19 17:00:31 +0600

    Create tab bar

---
+++ GHFollowers/FavoritesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesViewController.swift
+//  GHFollowers
+//
+//  Created by Yernar Masujima on 12/18/20.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+    }
+    
+}
